203ded7ba13aadf1b5610a049a215c1f2232d19d
algold2001 <45596152+algold2001@users.noreply.github.com>
2018-12-04 11:03:23 -0200

    Add files via upload

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(function (req, res, next) {
 
 function execSQLQuery(sqlQry, res) {
     const connection = mysql.createConnection({
-        host: "www.db4free",
+        host: "db4free.net",
         user: "cdahts1",
         port: "3306",
         password: "br4s1l2018",
